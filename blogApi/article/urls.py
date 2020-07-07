8ce173a3442cf53939221a661b461920013a999e
Akshat Zala <akshat.z@arnekt.com>
2020-07-07 20:29:28 +0530

    Reverted back to previous state

--- blogApi/article/urls.py
+++ blogApi/article/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-
-from .views import ArticleView, SingleArticleView
-
+from .views import ArticleDetail
 
 app_name = "articles"
 
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
-    path('articles/', ArticleView.as_view()),
-    path('articles/<int:pk>', SingleArticleView.as_view()),
+    path('articles/', ArticleDetail.as_view()),
 ]
--- blogApi/article/views.py
+++ blogApi/article/views.py
@@ -1,25 +1,36 @@
-from rest_framework.generics import get_object_or_404, GenericAPIView, CreateAPIView, ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from rest_framework import mixins
 from .models import Article, Author
 from .serializers import ArticleSerializer
 
-class ArticleView(ListCreateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-    
-    def perform_create(self, serializer):
-        author = get_object_or_404(Author, id=self.request.data.get('author_id'))
-        return serializer.save(author=author)
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, *kwargs)
+class ArticleDetail(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response({"articles": serializer.data})
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+    def post(self, request):
+        articles = request.data.get('articles')
+        serializer = ArticleSerializer(data=articles)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+        return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
+
+    def put(self, request, pk):
+        saved_article = get_object_or_404(Article.objects.all(), pk=pk)
+        data = request.data.get('article')
+        serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
+
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+        return Response({"success": "Article '{}' updated successfully".format(article_saved.title)})
 
 
-class SingleArticleView(RetrieveUpdateDestroyAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
+    def delete(self, request, pk):
+        # Get object with this pk
+        article = get_object_or_404(Article.objects.all(), pk=pk)
+        article.delete()
+        return Response({"message": "Article with id `{}` has been deleted.".format(pk)},status=204)
