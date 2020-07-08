96a03d964b48aadcfc5efa9356bea9d639996640
Akshat Zala <akshat.z@arnekt.com>
2020-07-08 17:30:06 +0530

    Added PUT/ GET methods

--- blogApi/article/views.py
+++ blogApi/article/views.py
@@ -1,42 +1,27 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import mixins
+from rest_framework import mixins, status, viewsets
+# from rest_framework.parsers import JSONParser
+
 from .models import Article, Author
 from .serializers import ArticleSerializer
 from django.shortcuts import get_object_or_404
 
+class ArticleDetail(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
 
-class ArticleDetail(APIView):
-    def get(self, request):
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return Response({"articles": serializer.data})
-
-class ArticleCreate(mixins.CreateModelMixin, generics.GenericAPIView):
-
-    queryset= Article.objects.all()
+    queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
+    def perform_create(self, serializer):
+        author = get_object_or_404(Author, id=self.request.data.get('author_id'))
+        return serializer.save(author=author)
+
     def get(self, request, *args, **kwargs):
-       return self.list(request, *args, **kwargs)
+        return self.list(request, *args, *kwargs)
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
-
-class ArticleUpdate(generics.GenericAPIView):
-    def put(self, request, pk):
-        saved_article = get_object_or_404(Article.objects.all(), pk=pk)
-        data = request.data.get('article')
-        serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
-
-        if serializer.is_valid(raise_exception=True):
-            article_saved = serializer.save()
-        return Response({"success": "Article '{}' updated successfully".format(article_saved.title)})
-
-class ArticleDelete(APIView):
-    def delete(self, request, pk):
-        # Get object with this pk
-        article = get_object_or_404(Article.objects.all(), pk=pk)
-        article.delete()
-        return Response({"message": "Article with id `{}` has been deleted.".format(pk)},status=204)
+class SingleArticleView(generics.RetrieveUpdateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
