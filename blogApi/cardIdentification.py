ef1607ca0589ce2ca1a57c937ddd0f6fe9186e0e
Akshat Zala <akshat.z@arnekt.com>
2020-07-10 21:34:25 +0530

    Working as OCR

--- blogApi/cardIdentification.py
+++ blogApi/cardIdentification.py
@@ -8,32 +8,33 @@ import re
 import imutils
 
 
-def preprocess(path):
-    img = cv.imread(path,0)
-    blurred = cv.blur(img, (3,3))
-    canny = cv.Canny(blurred, 50, 200)
-    pts = np.argwhere(canny>0)
-    y1,x1 = pts.min(axis=0)
-    y2,x2 = pts.max(axis=0)
-    cropped = img[y1:y2, x1:x2]
-    imS = imutils.resize(cropped, width=950)
-    cv.imwrite('/home/akshatz/Downloads/IMG_20200702_210159.jpg',imS);
-    image = Image.open('/home/akshatz/Downloads/IMG_20200702_210159.jpg')
-    enhancer = ImageEnhance.Brightness(image)
-    enhanced_im = enhancer.enhance(2)
-    con = ImageEnhance.Contrast(enhanced_im)
-    con1 = con.enhance(1.3)
-    enhancer_object = ImageEnhance.Sharpness(con1)
-    out = enhancer_object.enhance(3)
-    out.save("/home/akshatz/Downloads/IMG_20200702_210159.jpg")
-    #    out.show()
-    i = cv.imread('/home/akshatz/Downloads/IMG_20200702_210159.jpg',0)
-    ret, imgf = cv.threshold(i, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
-    cv.imwrite('/home/akshatz/Downloads/IMG_20200702_210159.jpg',imgf)
-    output = pytesseract.image_to_string(imgf, lang='eng')
-    with open("ID.txt", "a") as f:
-        f.write(output)
-    return(output)
+# def preprocess(path):
+    # img = cv.imread(path,0)
+    # blurred = cv.blur(img, (3,3))
+    # canny = cv.Canny(blurred, 50, 200)
+    # pts = np.argwhere(canny>0)
+    # y1,x1 = pts.min(axis=0)
+    # y2,x2 = pts.max(axis=0)
+    # cropped = img[y1:y2, x1:x2]
+    # imS = imutils.resize(cropped, width=950)
+    # cv.imwrite('/home/akshatz/Downloads/IMG_20200702_210159.jpg',imS);
+    # image = Image.open('/home/akshatz/Downloads/IMG_20200702_210159.jpg')
+    # enhancer = ImageEnhance.Brightness(image)
+    # enhanced_im = enhancer.enhance(2)
+    # con = ImageEnhance.Contrast(enhanced_im)
+    # con1 = con.enhance(1.3)
+    # enhancer_object = ImageEnhance.Sharpness(con1)
+    # out = enhancer_object.enhance(3)
+    # out.save("/home/akshatz/Downloads/IMG_20200702_210159.jpg")
+    # #    out.show()
+    # i = cv.imread('/home/akshatz/Downloads/IMG_20200702_210159.jpg',0)
+    # ret, imgf = cv.threshold(i, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+    # cv.imwrite('/home/akshatz/Downloads/IMG_20200702_210159.jpg',imgf)
+    # output = pytesseract.image_to_string(imgf, lang='eng')
+    # with open("ID.txt", "a") as f:
+    #     f.write(output)
+    #     print(output)
+    # return(output)
 
 
 def AADHARproc(out):
@@ -59,9 +60,13 @@ def AADHARproc(out):
     else:
         return num.group(1)+" AADHAR CARD"
 
-def rear(request):
-    try:
-        file = '/home/akshatz/Downloads/e1.jpg'
-        return(AADHARproc(preprocess(file)))
-    except:
-        return None
+def rear():
+    # print("Hello")
+    # try:
+    filename = '/home/akshatz/Downloads/IMG_20200702_210159 (2).jpg'
+    #     return(AADHARproc(preprocess(file)))
+    # except:
+    #     return None
+    text = str(((pytesseract.image_to_string(Image.open(filename))))) 
+    print(text)
+rear()
