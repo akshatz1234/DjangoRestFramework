75ec7297645302e1280c2e9d712940497e2c9d08
Akshat Zala <akshat.z@arnekt.com>
2020-07-10 18:04:55 +0530

    Added postProcess

--- blogApi/postProcessID.py
+++ blogApi/postProcessID.py
@@ -1,4 +1,12 @@
-with open("ID.txt", "r") as fi:
-    with open("Id1.txt", "a") as fo:
-        f = fi.seek()
-        print(fi.read())
+import re
+fi = "ID.txt"
+fo = "Id1.txt"
+with open(fi, "r") as fi:
+    with open(fo, "a") as fo:
+        str(fi.read())
+        if re.search("^[a-zA-Z\. ]+$", str(fi.read())):
+            print(str(fi.read()))   
+            name = re.search("^[a-zA-Z\. ]+$")
+            name = str(name)
+            print(name)
+            fo.write(name)
