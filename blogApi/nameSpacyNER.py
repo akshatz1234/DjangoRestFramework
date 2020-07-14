9656e80c4dc82d681e548e8a143096eff7c59c58
Akshat Zala <akshat.z@arnekt.com>
2020-07-14 13:06:42 +0530

    Created dictionary

--- blogApi/nameSpacyNER.py
+++ blogApi/nameSpacyNER.py
@@ -1,13 +1,32 @@
-import spacy
-from spacy.matcher import Matcher
+from nltk.tokenize import word_tokenize
+import nltk
+import re
+text = open("ID.txt").read()
 
-
-nlp = spacy.load('en_core_web_sm')
-matcher = Matcher(nlp.vocab)
-
-fileName = "ID.txt" 
-
-introductionFileText = open(fileName).read()
-doc = nlp(introductionFileText)
-for token in doc:
-    print(token)
+tokenized_text = word_tokenize(text)
+n = nltk.pos_tag(tokenized_text)
+# for i in range(n):
+	# print(n[i])
+# query = [lis[1] for lis in n]
+# result = [lis[0] for lis in n]
+# query =  ", ".join(map(str, query))
+# print(query)
+# result = ", ".join(map(str, result))
+# print(query, result)
+# print(type(result))
+# print(result)
+# print(text)
+# print(n)
+# if query.find('NNP'):
+# 	print(result)
+# print(n)
+dictionary = dict()
+# print(tokenized_text)
+for name, typeofText in n:
+	dictionary.setdefault(name, []).append(typeofText)
+# print(dictionary)
+# if dict_values == "NNP" or dict_values == "NNP, NNP":
+# 	print(dict.keys)
+for key, value in my_dict.items(): 
+         if val == value: 
+             return key 
