43ed2f92ec5782e118ec99b436d90c0683c97a70
Akshat Zala <akshat.z@arnekt.com>
2020-07-14 16:00:13 +0530

    Added for PAN

--- blogApi/nameSpacyNER.py
+++ blogApi/nameSpacyNER.py
@@ -5,28 +5,12 @@ text = open("ID.txt").read()
 
 tokenized_text = word_tokenize(text)
 n = nltk.pos_tag(tokenized_text)
-# for i in range(n):
-	# print(n[i])
-# query = [lis[1] for lis in n]
-# result = [lis[0] for lis in n]
-# query =  ", ".join(map(str, query))
-# print(query)
-# result = ", ".join(map(str, result))
-# print(query, result)
-# print(type(result))
-# print(result)
-# print(text)
-# print(n)
-# if query.find('NNP'):
-# 	print(result)
 # print(n)
 dictionary = dict()
 # print(tokenized_text)
 for name, typeofText in n:
-	dictionary.setdefault(name, []).append(typeofText)
-# print(dictionary)
-# if dict_values == "NNP" or dict_values == "NNP, NNP":
-# 	print(dict.keys)
-for key, value in my_dict.items(): 
-         if val == value: 
-             return key 
+	dictionary.setdefault(name, typeofText)
+
+for key, value in dictionary.items():
+	if 'NNP' in value:
+		print(key)
