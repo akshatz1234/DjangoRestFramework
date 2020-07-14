2eaba75f0fd06e3bdfc803eaf222f1fdb9ff28ae
Akshat Zala <akshat.z@arnekt.com>
2020-07-14 17:54:51 +0530

    Added for PAN

--- blogApi/nameSpacyNER.py
+++ blogApi/nameSpacyNER.py
@@ -2,15 +2,20 @@ from nltk.tokenize import word_tokenize
 import nltk
 import re
 text = open("ID.txt").read()
-
+# text = "JATIN PRAFULBHAI ZALA"
 tokenized_text = word_tokenize(text)
 n = nltk.pos_tag(tokenized_text)
-# print(n)
 dictionary = dict()
-# print(tokenized_text)
+print(n)
+# print(dictionary)
 for name, typeofText in n:
 	dictionary.setdefault(name, typeofText)
-
+	# print(dictionary)
 for key, value in dictionary.items():
-	if 'NNP' in value:
-		print(key)
+	with open("id.txt", "a") as f:
+		if "NNP" in value:
+			print(key, end=" ")
+			f.write(key+" ")
+		elif "CD" in value:
+			f.write("\n")
+			f.write("Age= "+ key + "\n")
