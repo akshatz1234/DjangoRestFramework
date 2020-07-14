7f0e30ddb4a70ae3c2307990ed98ca0ace795184
Akshat Zala <akshat.z@arnekt.com>
2020-07-14 19:40:29 +0530

    Tue 14 Jul 19:40:29 IST 2020

--- blogApi/nameSpacyNER.py
+++ blogApi/nameSpacyNER.py
@@ -1,21 +1,21 @@
 from nltk.tokenize import word_tokenize
 import nltk
-import re
 text = open("ID.txt").read()
-# text = "JATIN PRAFULBHAI ZALA"
 tokenized_text = word_tokenize(text)
 n = nltk.pos_tag(tokenized_text)
 dictionary = dict()
-print(n)
+# print(n)
 # print(dictionary)
 for name, typeofText in n:
 	dictionary.setdefault(name, typeofText)
 	# print(dictionary)
 for key, value in dictionary.items():
+	# FOR PAN CARD DETECTION 
 	with open("id.txt", "a") as f:
 		if "NNP" in value:
 			print(key, end=" ")
 			f.write(key+" ")
 		elif "CD" in value:
 			f.write("\n")
-			f.write("Age= "+ key + "\n")
+			f.write("Age= "+ key)
+			f.write("\n")
