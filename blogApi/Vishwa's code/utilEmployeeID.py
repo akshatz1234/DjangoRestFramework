92478c088de242799f418fb2cef44b87958f985b
Akshat Zala <akshat.z@arnekt.com>
2020-07-17 18:49:51 +0530

    Bloodgroup

---
+++ blogApi/Vishwa's code/utilEmployeeID.py
@@ -0,0 +1,67 @@
+
+from nltk.tag.stanford import StanfordNERTagger
+from nltk.tokenize import word_tokenize
+import re
+from itertools import groupby
+from flask import jsonify
+import util_age
+
+
+st = StanfordNERTagger('/home/akshatz/Documents/stanford-ner-4.0.0/classifiers/english.conll.4class.distsim.crf.ser.gz',
+                       '/home/akshatz/Documents/stanford-ner-4.0.0/stanford-ner.jar',
+                       encoding='utf-8')
+
+
+
+def nameex(txt):
+    """
+    In: Classified text from StanfordNER
+    Out: Name
+    """
+    for tag, chunk in groupby(txt, lambda x:x[1]):
+        if tag != "O" and tag == 'PERSON':
+            a=" ".join(w for w, t in chunk)
+            return(a)
+            break
+
+def dateex(output):
+    """
+    In: Output from the OCR
+    out: Date
+    """
+    date = re.search("([0-9]{2}\/[0-9]{2}\/[0-9]{4}|[0-9]{2}\-[0-9]{2}\-[0-9]{4})", output)
+    if date is None:
+        year = re.search("(19..|20..)", output)
+        if year is None:
+            return "None"
+        else:
+            return(year.group(1))
+    else:
+        return(date.group(1))
+        
+
+def age(dob):
+    if dob == "None":
+        return "None"
+    else:
+        return(util_age.main(dob))
+        
+# bloodgroup 
+def bloodGroup(output):
+    blood_group= re.search("^(A|B|AB|O)[+-]$")
+    if bloodGroup is None:
+        return None
+    else: 
+        return (bloodGroup.group(1))
+    
+
+def main_ex(output):
+    tokenized_text = word_tokenize(output)
+    classified_text = st.tag(tokenized_text)
+    print(classified_text)
+    data = {}
+    data['name'] = nameex(classified_text)
+    data['dob'] = dateex(output)
+    data['age'] = age(data['dob'])
+    data['docType'] = "PAN_card"
+    return jsonify(data)
